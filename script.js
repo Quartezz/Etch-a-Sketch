7ae6dbab293a17465f4279eacf02daf6e437f42f
Quartezz <kamil.machel03@gmail.com>
2023-08-05 19:21:26 +0200

    Solved the exercise

--- script.js
+++ script.js
@@ -21,8 +21,87 @@ function setCurrentMode(newMode) {
 const grid = document.getElementById('grid');
 const sizeSlider = document.getElementById('sizeSlider');
 const sizeValue = document.getElementById('sizeValue');
-const clearBtn = document.getElementById('clear-button');
-const rainbowBtn = document.getElementById('rainbow-button');
-const eraserBtn = document.getElementById('eraser-button');
-const colorPicker = document.getElementById('colorPicker');
-const colorBtn = document.getElementById('color-button')
+const clearBtn = document.getElementById('clearBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const eraserBtn = document.getElementById('eraserBtn');
+const colorPicker = document.getElementById('colorBtn');
+const colorBtn = document.getElementById('colorBtn');
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode('color');
+rainbowBtn.onclick = () => setCurrentMode('rainbow');
+eraserBtn.onclick = () => setCurrentMode('eraser');
+clearBtn.onclick = () => reloadGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+let mouseDown = false;
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid();
+}
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+}
+
+function reloadGrid() {
+    clearGrid();
+    setupGrid(currentSize);
+}
+
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement('div')
+      gridElement.classList.add('square')
+      gridElement.addEventListener('mouseover', changeColor)
+      gridElement.addEventListener('mousedown', changeColor)
+      grid.appendChild(gridElement)
+    }
+  }
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if (currentMode === 'rainbow') {
+      const randomR = Math.floor(Math.random() * 256);
+      const randomG = Math.floor(Math.random() * 256);
+      const randomB = Math.floor(Math.random() * 256);
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'color') {
+      e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#fefefe'
+    }
+  }
+  
+function activateButton(newMode) {
+    if (currentMode === 'rainbow') {
+      rainbowBtn.classList.remove('active')
+    } else if (currentMode === 'color') {
+      colorBtn.classList.remove('active')
+    } else if (currentMode === 'eraser') {
+      eraserBtn.classList.remove('active')
+    }
+  
+    if (newMode === 'rainbow') {
+      rainbowBtn.classList.add('active')
+    } else if (newMode === 'color') {
+      colorBtn.classList.add('active')
+    } else if (newMode === 'eraser') {
+      eraserBtn.classList.add('active')
+    }
+  }
+  
+  window.onload = () => {
+    setupGrid(DEFAULT_SIZE)
+    activateButton(DEFAULT_MODE)
+  }
+  
