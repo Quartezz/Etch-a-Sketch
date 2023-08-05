1d5d7316046983a675afc94955109265b14f0602
Quartezz <kamil.machel03@gmail.com>
2023-08-05 14:45:05 +0200

    created function to create 16x16 grids inside the grid-container class

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const gridContainer = document.querySelector('.grid-container');
+
+function createGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        gridContainer.appendChild(square);
+    }
+}
+
+createGrid(16);
