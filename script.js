4f565adb67a6f8fd58c92287b85c8cb0172b126d
Quartezz <kamil.machel03@gmail.com>
2023-08-31 15:36:56 +0200

    Solved the order of the functions that caused the clear button to not work

--- script.js
+++ script.js
@@ -27,6 +27,29 @@ const eraserBtn = document.getElementById('eraserBtn');
 const colorPicker = document.getElementById('colorBtn');
 const colorBtn = document.getElementById('colorBtn');
 
+function clearGrid() {
+  grid.innerHTML = '';
+}
+
+function setupGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div')
+    gridElement.classList.add('square')
+    gridElement.addEventListener('mouseover', changeColor)
+    gridElement.addEventListener('mousedown', changeColor)
+    grid.appendChild(gridElement);
+  }
+}
+
+function reloadGrid() {
+  clearGrid();
+  setupGrid(currentSize);
+}
+
+
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
 colorBtn.onclick = () => setCurrentMode('color');
 rainbowBtn.onclick = () => setCurrentMode('rainbow');
@@ -40,33 +63,20 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+
+
 function changeSize(value) {
     setCurrentSize(value);
     updateSizeValue(value);
     reloadGrid();
 }
 
+
+
 function updateSizeValue(value) {
     sizeValue.innerHTML = `${value} x ${value}`
 }
 
-function reloadGrid() {
-    clearGrid();
-    setupGrid(currentSize);
-}
-
-function setupGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-  
-    for (let i = 0; i < size * size; i++) {
-      const gridElement = document.createElement('div')
-      gridElement.classList.add('square')
-      gridElement.addEventListener('mouseover', changeColor)
-      gridElement.addEventListener('mousedown', changeColor)
-      grid.appendChild(gridElement)
-    }
-  }
 
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
